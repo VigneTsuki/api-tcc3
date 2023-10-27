dc207526bdff13c6d5213b6f0be7a8d1d158d032
VigneTsuki <vigne.kirara.jp@gmail.com>
2023-10-27 20:56:50 -0300

    mostrar swagger mesmo em producao

--- src/PresencaAutomatizada.Application/Program.cs
+++ src/PresencaAutomatizada.Application/Program.cs
@@ -33,11 +33,11 @@ app.UseCors(builder =>
 });
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 //app.UseHttpsRedirection();
 
